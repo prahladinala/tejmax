39227c4354f5c89c19ea8aaf921479419e8fcc3e
prahladinala <prahlad2018@gmail.com>
2021-04-28 12:23:29 +0530

    Padding and Margins set

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,7 +8,7 @@ const MovieCard = ({
 }) => {
   return (
     <div className='col-lg-3 col-6'>
-      <div className='card'>
+      <div className='card mt-2'>
         <img src={poster} className='card-img-top' alt='...' />
         <div className='card-body'>
           <h5 className='card-title'>
