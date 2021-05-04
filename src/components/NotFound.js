806ac76b9abe03595a5f1a752cef6695b00dbd6b
prahladinala <prahlad2018@gmail.com>
2021-05-04 20:29:39 +0530

    Designed Admin Panel and 404 not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import notfound from "../img/404.png"
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Page Not Found</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-6  .offset-md-3  .offset-lg-3">
+          <div className="text-center">
+            <img className="img-fluid" src={notfound} />
+            <Link to="/" className="btn btn-info">Go back to Home Page</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
