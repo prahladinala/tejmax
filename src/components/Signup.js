a063f598ffaa6adc7b6d93d9d894d2f0b2ab10da
prahladinala <prahlad2018@gmail.com>
2021-05-04 11:15:22 +0530

    Signup form all inputs are important

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -49,24 +49,24 @@ const Signup = () => {
                 <form>
                     <div class="form-group">
                         <label for="exampleInputFirstName">First Name</label>
-                        <input type="text" onChange={handleChange("firstName")} class="form-control" id="exampleInputFirstName" aria-describedby="emailHelp" placeholder="First Name" value={firstName} />
+                        <input type="text" onChange={handleChange("firstName")} required class="form-control" id="exampleInputFirstName" aria-describedby="emailHelp" placeholder="First Name" value={firstName} />
                     </div>
                     <div class="form-group">
                         <label for="exampleInputLastName">Last Name</label>
-                        <input type="text" onChange={handleChange("lastName")} class="form-control" id="exampleInputLastName" aria-describedby="emailHelp" placeholder="Last Name" value={lastName} />
+                        <input type="text" onChange={handleChange("lastName")} required class="form-control" id="exampleInputLastName" aria-describedby="emailHelp" placeholder="Last Name" value={lastName} />
                     </div>
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" onChange={handleChange("email")} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={email} />
+                        <input type="email" onChange={handleChange("email")} required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={email} />
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
-                        <input type="password" onChange={handleChange("password")} class="form-control" id="exampleInputPassword1" placeholder="Password" value={password} />
+                        <input type="password" onChange={handleChange("password")} required class="form-control" id="exampleInputPassword1" placeholder="Password" value={password} />
                     </div>
                     <div class="form-group">
                         <label for="exampleInputPhone">Phone Number</label>
-                        <input type="text" onChange={handleChange("phone")} class="form-control" id="exampleInputPhone" aria-describedby="emailHelp" placeholder="Phone Number" value={phone} />
+                        <input type="text" onChange={handleChange("phone")} required class="form-control" id="exampleInputPhone" aria-describedby="emailHelp" placeholder="Phone Number" value={phone} />
                     </div>
                     <button onClick={onSubmit} class="btn btn-primary">Sign Up</button>
                 </form>
